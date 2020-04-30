afe71fe3b969376a356b2861171d40d5999f3fca
fracerma <fracerma@users.github.com>
2020-04-30 18:13:44 +0200

    Basic User, login and session

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+
+  resource :user, except: [:new]
+  resolve('User') { [:user] }
+  resources :sessions, only: [:create, :destroy]
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'pages#login', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
