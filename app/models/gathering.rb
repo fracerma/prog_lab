f653db0993877f8c200bb1ea1a30a02e3792624d
Michela Capasso <michelacapasso@MBPdiMichela2.homenet.telecomitalia.it>
2020-05-10 20:08:17 +0200

    funzione generate location

--- app/models/gathering.rb
+++ app/models/gathering.rb
@@ -4,12 +4,7 @@ class Gathering < ApplicationRecord
     has_many :groups
     has_many :users, :through => :groups
 
+    
 
-    # metodo di prova per aggiungere una lista di utenti a un gruppo per questa uscita
-    def new_group(utenti)
-        utenti.each do |n|
-            self.users << n
-            #magari qui possiamo aggiunger che manda una notifica ad ognuno di questi
-        end
-    end
+    
 end
